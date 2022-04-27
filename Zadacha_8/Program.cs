d0ebce600e468af0cdedb7c4c81d47ffc6116f2d
«Zheka <zhekakad@ya.ru>
2022-04-27 14:16:24 +0900

    решение восьмой задачи

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите проверямое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите делитель: "); 
+int number1 = int.Parse(Console.ReadLine()); 
+if (number % number1 == 0)
+{
+    Console.WriteLine("Число: "+number+" кратно числу "+number1+"");
+}
+else
+{   
+    int remains = number % number1; 
+    Console.WriteLine("Число " + number + " не кратно числу "+number1+"");
+    Console.WriteLine("Остаток от деления: "+remains+"");
+}
