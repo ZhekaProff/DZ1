404281a6750f8c28ba5f68893b766d1b969d1f59
«Zheka <zhekakad@ya.ru>
2022-04-27 13:14:10 +0900

    решение шестой задачи

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.WriteLine("Введите число от 10 до 99");
+int count = int.Parse(Console.ReadLine());
+Console.Write("Наибольшая цифра: ");
+int count1 = count % 10;
+int count2 = count / 10;
+    if ( count1 >= count2)
+    {
+        Console.Write(count1);
+    }
+else
+{
+    Console.Write(count2);
+}
