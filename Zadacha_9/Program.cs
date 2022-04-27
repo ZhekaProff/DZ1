95fbeb00b4152eb442cbc4b7844b0d4f0786186d
«Zheka <zhekakad@ya.ru>
2022-04-27 20:58:31 +0900

    решение девятой задачи неполное

---
+++ Zadacha_9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число: ");
+int count = int.Parse(Console.ReadLine());
+if ( count > 99)
+{
+  if(count >99 && count<= 999);
+    {
+    Console.WriteLine("Третья цифра: "+count%10+""); 
+    }
+}
+else
+{
+    Console.WriteLine("У числа "+count+" нет третьей цифры!");
+}
