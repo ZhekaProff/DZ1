01f956c3b08f0311a0d6ccc665c82eac00af374f
«Zheka <zhekakad@ya.ru>
2022-04-27 11:07:37 +0900

    решение третьей задачи

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Число четное! ");
+}
+else
+{
+    Console.Write("Число не четное!");
+}
