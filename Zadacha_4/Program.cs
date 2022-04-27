477ba8700c6b1b1ad9e140349c406cdbc162dad9
«Zheka <zhekakad@ya.ru>
2022-04-27 12:10:14 +0900

    решение четвертой задачи

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 2;
+while ( i <= n)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    i = i + 2;
+}
