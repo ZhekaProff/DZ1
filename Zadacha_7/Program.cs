144794f79cf76b53569a30af3300138e03837b98
«Zheka <zhekakad@ya.ru>
2022-04-27 13:40:54 +0900

    решение судьмой задачи

---
+++ Zadacha_7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзнчное число: ");
+int count = int.Parse(Console.ReadLine());
+int count1 = count / 100;
+int count2 = count % 10;
+Console.WriteLine("Ваше новое число: "+count1+""+count2+"");
