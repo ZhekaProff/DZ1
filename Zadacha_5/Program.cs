584dc47916c58166bac5fb53c7cd440623735192
«Zheka <zhekakad@ya.ru>
2022-04-27 12:23:11 +0900

    решение пятой задачи

---
+++ Zadacha_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a > 99 && a < 1000)
+{
+Console.Write("Вторая цифра 3-х значного числа = ");
+Console.WriteLine((a / 10) % 10);
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное!");
+}
